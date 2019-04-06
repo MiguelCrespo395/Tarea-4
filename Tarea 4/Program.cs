56028b176ed3608fbf48fdf5cd2467ae917be4ed
MiguelCrespo395 <47574010+MiguelCrespo395@users.noreply.github.com>
2019-04-05 20:04:47 -0700

    Add files via upload

---
+++ Tarea 4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Tarea 4
+
+{
+    abstract class Musico
+    {
+        public string nombre;
+        public Musico(string n)
+        {
+            nombre = n;
+        }
+        public virtual void Afina() { }
+        public string GetNombre()
+        {
+            return nombre;
+        }
+    }
+    class Bajista : Musico
+    {
+        public string instrumento;
+        public Bajista(string n, string i): base(n)
+        {
+            instrumento = i;
+        }
+	    public override void Afina()
+        {
+            Console.WriteLine("Afiando el {0}", instrumento);
+        }
+    }
+
+        class Guitarrista : Musico
+        {
+            public string instrumento;
+
+            public Guitarrista(string n, string i) :base(n)
+            {
+                instrumento = i;
+            }
+
+            public override void Afina()
+            {
+                Console.WriteLine("Afiando el {0}", instrumento);
+            }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bajista b = new Bajista("Flea", "Bajo");
+            Guitarrista g = new Guitarrista("Santana", "Guitarra");
+
+            b.Afina();
+            g.Afina();
+
+            Console.ReadKey();
+        }
+    }
+}
